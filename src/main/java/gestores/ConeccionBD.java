da06bf6d1fc66661afd058022f45b3f278fbb70d
Stephany Morzan <s.a.m.fuentes08@gmail.com>
2015-09-21 17:47:39 -0500

    Se puso backend hecho por Gary. Errores corregidos. Llave de shit again AND AGAIN

--- src/main/java/gestores/ConeccionBD.java
+++ src/main/java/gestores/ConeccionBD.java
@@ -24,6 +24,11 @@ public class ConeccionBD {
             String password = dbUri.getUserInfo().split(":")[1];
             String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
+            username="bd6a400a5871b4";
+            password="22857d4d";
+            dbUrl="jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_f195b040bc3b337";
+
+
             return DriverManager.getConnection(dbUrl, username, password);
         } catch (Exception ex) {
             conexion = null;
