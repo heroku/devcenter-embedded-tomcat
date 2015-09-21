de0d3efafedce0ef5c61994051c43aa9fc0f4bc1
Stephany Morzan <s.a.m.fuentes08@gmail.com>
2015-09-21 16:49:58 -0500

    Test

---
+++ src/main/java/servlet/TestServlet.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        name = "ServletWii",
+        urlPatterns = {"/test"}
+    )
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hola heroku de shit".getBytes());
+        out.flush();
+        out.close();
+    }
+    
+}
