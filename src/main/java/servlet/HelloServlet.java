d3d61f1837dfc5c57a9a6815e3492dd3dcb94a43
Stephany Morzan <s.a.m.fuentes08@gmail.com>
2015-09-21 16:42:18 -0500

    Hola

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
+        out.write("hola heroku".getBytes());
         out.flush();
         out.close();
     }
