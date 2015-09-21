722edd1ec27f386880931f777d15ca5565dd4476
Stephany Morzan <s.a.m.fuentes08@gmail.com>
2015-09-21 17:31:28 -0500

    Se puso backend hecho por Gary. Errores

--- src/main/java/servlet/UserRegistroServlet.java
+++ src/main/java/servlet/UserRegistroServlet.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by stmorzan on 21/09/2015.
