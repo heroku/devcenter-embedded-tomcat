70b3a9e5813cbec44b5a879200169ded69587625
Falcokun <pst.falco@gmail.com>
2015-09-22 00:43:56 -0500

    TestServlet

--- src/main/java/gestores/ConeccionBD.java
+++ src/main/java/gestores/ConeccionBD.java
@@ -14,17 +14,12 @@ public class ConeccionBD {
     public static Connection GetConnection() {
         Connection conexion = null;
         try {
-            /*
+
             URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
             String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-*/
-
-            String username = "bd6a400a5871b4";
-            String password = "22857d4d";
-            String dbUrl = "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net";
 
             Class.forName("com.mysql.jdbc.Driver");
 
--- src/main/java/servlet/TestServlet.java
+++ src/main/java/servlet/TestServlet.java
@@ -21,26 +21,6 @@ public class TestServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
 
-        URI dbUri = null;
-        try {
-            dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-
-            out.write(username.getBytes());
-            out.write("|||".getBytes());
-            out.write(password.getBytes());
-            out.write("|||".getBytes());
-            out.write(dbUrl.getBytes());
-            out.write("|||".getBytes());
-
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-
-
         out.write("hola heroku de shit".getBytes());
         out.flush();
         out.close();
