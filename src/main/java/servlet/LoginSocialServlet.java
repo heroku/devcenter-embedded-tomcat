dbbdc2b3bba5403d5d1b7c29e821486ec2f80a5b
Gary1453 <gary_cd10@hotmail.com>
2015-10-20 00:50:36 -0500

    subiendo Servlets

---
+++ src/main/java/servlet/LoginSocialServlet.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(
+name = "LoginServlet",
+urlPatterns = "/usuario/login"
+)
+
+
+public class LoginSocialServlet extends HttpServlet{
+    
+    
+ @Override       
+ protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+ 
+ }    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/servlet/SeleccionarGustosServlet.java
@@ -0,0 +1,34 @@
+
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+name ="",
+urlPatterns = ""
+
+)
+
+public class SeleccionarGustosServlet extends HttpServlet{
+    
+    
+@Override
+ protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+ 
+ 
+ }
+
+ 
+    
+    
+    
+    
+    
+}
