5a7b25ea20db9d215d00d80032c5e68ee8b4eb9e
Gary1453 <gary_cd10@hotmail.com>
2015-11-09 12:35:47 -0500

    agregado clase conexionauxiliar

--- src/main/java/gestores/ConexionAuxiliar.java
+++ src/main/java/gestores/ConexionAuxiliar.java
@@ -1,38 +1,46 @@
 
 package gestores;
 
-import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class ConexionAuxiliar {
-    
-        public static Connection GetConnection() {
-        Connection conexion;
+
+    public static Connection GetConnection() {
+        Connection conexion = null;
+
         try {
-
-            URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-
             Class.forName("com.mysql.jdbc.Driver");
+            String servidor = "jdbc:mysql://www.db4free.net:3306/foodtracker";
+            String usuarioDB = "b3693d12c2c8c9";
+            String passwordDB = "123f4e4b";
+            conexion = DriverManager.getConnection(servidor, usuarioDB, passwordDB);
 
-            conexion = DriverManager.getConnection(dbUrl, username, password);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex, "Error1 en la Conexión con la BD " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
             conexion = null;
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, ex, "Error2 en la Conexión con la BD " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion = null;
+
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, ex, "Error3 en la Conexión con la BD " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion = null;
+        } finally {
+
+            return conexion;
+
         }
-        return conexion;
     }
+
     
     
     
     
     
     
-    
-    
-}
+     }
