36bbbc8ed13cb33ee30291baeae178e99d09861d
Vladimir Tyurin <bfg.tyurin@gmail.com>
2015-11-12 00:32:10 +0300

    Servlet 3.0 annotations work and update tomcat

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,6 +1,11 @@
 package launch;
 import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 public class Main {
 
@@ -18,8 +23,16 @@ public class Main {
 
         tomcat.setPort(Integer.valueOf(webPort));
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
 
         tomcat.start();
         tomcat.getServer().await();  
