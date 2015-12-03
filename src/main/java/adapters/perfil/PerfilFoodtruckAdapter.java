ddcbb0000f4bd32ff1080a70761c6864e44fb3c6
Stephy <s.a.m.fuentes08@gmail.com>
2015-12-03 17:49:27 -0500

    comentado

--- src/main/java/adapters/perfil/PerfilFoodtruckAdapter.java
+++ src/main/java/adapters/perfil/PerfilFoodtruckAdapter.java
@@ -6,37 +6,29 @@ import dto.UserRegistro.FoodTruckResponse;
 import gestores.GestorBD;
 
 
-public class PerfilFoodtruckAdapter implements PerfilAdapter{
+public class PerfilFoodtruckAdapter implements PerfilAdapter {
 
-    public ClienteResponse mostrarPerfil(int idCliente,int idTipo) {
-   
-    GestorBD gestor=GestorBD.getInstance();
-    ClienteResponse clienteFoodTruck=gestor.mostrarPerfilCliente(idCliente,idTipo);
+    public ClienteResponse mostrarPerfil(int idCliente, int idTipo) {
+
+        GestorBD gestor = GestorBD.getInstance();
+
+        /*
+        ClienteResponse clienteFoodTruck=gestor.mostrarPerfilCliente(idCliente,idTipo);
     
     FoodTruckResponse foodtruck=new FoodTruckResponse();
     foodtruck.setIdFoodTruck(clienteFoodTruck.getId_Cliente());
     
     gestor.obtenerFoodTruck(foodtruck);
+
     return  clienteFoodTruck;
-    
+    */
+        return null;
     }
 
-    
-    
-    
+
     public void editarPerfil() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
