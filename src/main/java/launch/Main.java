51b73f85a96fea73535e672a4852932c9c60d280
Gary1453 <gary_cd10@hotmail.com>
2015-12-04 01:47:19 -0500

    ultima version

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -13,7 +13,7 @@ public class Main {
         //Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
-            webPort = "9999";
+            webPort = "8080";
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
